6c1331d4a93907199e9424b57efab2d311816771
Lewis1Ndungu <lewis.ndungu@student.moringaschool.com>
2022-12-01 08:02:00 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+
+function testFunc() {
+    return "hi"
+  }
+  
+  function superbowlWin(objArr) {
+    const winYear = objArr.find( e => e.result === "W")
+    if(winYear) {
+      return winYear.year
+    } else {
+      return undefined
+    }
+  }
